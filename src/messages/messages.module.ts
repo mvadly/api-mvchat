bbba41674e194d1eb1b525a85701caceb7ffbea6
MVChat Dev <dev@mvchat.app>
2026-04-26 09:57:04 +0700

    fix: add ConversationsModule import to MessagesModule

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
+import { ConversationsModule } from '../conversations/conversations.module';
 
 @Module({
+  imports: [ConversationsModule],
   controllers: [MessagesController],
   providers: [MessagesService],
   exports: [MessagesService],
